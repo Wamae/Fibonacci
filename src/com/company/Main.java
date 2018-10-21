06abb54f87d5eaaf6483cc8eee1625a69f9ff560
Wamae <wamaebenson06@gmail.com>
2018-10-21 10:14:09 +0300

    Fibonnaci

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.print( fib(i)+",");
+        }
+    }
+
+    private static int fib(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
