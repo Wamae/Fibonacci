ce27d416353ee1f674e5a9e51e6f0c78f5db8d45
Wamae <wamaebenson06@gmail.com>
2018-10-21 16:33:31 +0300

    Memoized solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         int n = 10;
+        int[] memo = new int[n+1];
+
         for (int i = 0; i < n; i++) {
-            System.out.print( fib(i)+",");
+            System.out.print( fib(i,memo)+",");
         }
     }
 
-    private static int fib(int n) {
+    private static int fib(int n,int[] memo) {
         if (n <= 0) {
             return 0;
         } else if (n == 1) {
             return 1;
+        }else if(memo[n]>0){
+            return memo[n];
         } else {
-            return fib(n - 1) + fib(n - 2);
+            return fib(n - 1,memo) + fib(n - 2,memo);
         }
     }
 }
